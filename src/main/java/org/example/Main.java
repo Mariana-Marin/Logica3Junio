7794480b4808b5072e7ec4216707f96090833e54
Mariana <marianamarinvanegas2014@gmail.com>
2025-06-03 10:05:09 -0500

    se crea proyecto y se inicializan variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        //Entradas del algoritmo
+
+        Scanner sc = new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+
+        String nombreUsusario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamento;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacio;
+        String utensiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte numeroBloque;
+        byte cantidadAsistentes;
+        Integer valorReserva;
+
+        boolean esAdmin = false;
+        boolean confirmacionReserva =false;
+        boolean estaOcupado = false;
+        boolean estaEnMantenimiento = false;
+
+
+
+
+
+        }
+    }
+}
